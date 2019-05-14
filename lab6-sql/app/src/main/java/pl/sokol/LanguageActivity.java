2f3d651a140a2985ae145995ef4b221570bf9302
f4lco0n <pawel_sokol@o2.pl>
2019-05-14 21:21:45 +0200

    added async

--- lab6-sql/app/src/main/java/pl/sokol/LanguageActivity.java
+++ lab6-sql/app/src/main/java/pl/sokol/LanguageActivity.java
@@ -1,13 +1,17 @@
 package pl.sokol;
 
 import android.app.Activity;
+import android.content.ContentValues;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteException;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -48,8 +52,46 @@ public class LanguageActivity extends Activity {
             Toast toast = Toast.makeText(this,"Baza danych jest niedostepna (LanguageActivity)",Toast.LENGTH_SHORT);
             toast.show();
         }
-
-
-
     }
+
+    public void onFavoriteClicked(View view){
+        int languageNo = (Integer)getIntent().getExtras().get("languageNo");
+        new UpdateLanguageTask().execute(languageNo);
+    }
+
+    private class UpdateLanguageTask extends AsyncTask<Integer, Void, Boolean>{
+
+        ContentValues languageValues;
+
+        @Override
+        protected void onPreExecute() {
+            CheckBox favorite = (CheckBox)findViewById(R.id.favorite);
+            languageValues = new ContentValues();
+            languageValues.put("FAVORITE",favorite.isChecked());
+        }
+
+        @Override
+        protected Boolean doInBackground(Integer... languages) {
+            int languageNo = languages[0];
+            SQLiteOpenHelper databaseHelper = new DatabaseHelper(LanguageActivity.this);
+            try {
+                SQLiteDatabase db = databaseHelper.getWritableDatabase();
+                db.update("LANGUAGE",languageValues,
+                                "_id = ?", new String[] {Integer.toString(languageNo)});
+                db.close();
+                return true;
+            } catch (SQLiteException e) {
+                return false;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(Boolean aBoolean) {
+            if(!aBoolean){
+                Toast toast = Toast.makeText(LanguageActivity.this,"Baza jest niedostepna (onPostExecute)",Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        }
+    }
+
 }
